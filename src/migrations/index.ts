beaca82da3f4092420aeced3798d25d43070ccba
Joseph Owonvwon <joseph.owonwo@gmail.com>
2025-10-19 18:28:08 +0100

    chore: initial migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251019_172658 from './20251019_172658';
+
+export const migrations = [
+  {
+    up: migration_20251019_172658.up,
+    down: migration_20251019_172658.down,
+    name: '20251019_172658'
+  },
+];
