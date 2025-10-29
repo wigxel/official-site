8f12cefd26d9153de1cd7c55ccf53d250aae3666
Joseph Owonvwon <joseph.owonwo@gmail.com>
2025-10-29 22:05:22 +0100

    chore: eslint fixes

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -1,8 +1,7 @@
 'use client'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import type React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useEffectEvent, useState } from 'react'
 import { Container } from '@/components/container'
 import { Logo } from '@/components/Logo/Logo'
 import type { Header } from '@/payload-types'
@@ -17,24 +16,29 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   /* Storing the value in a useState to avoid hydration errors */
   const [theme, setTheme] = useState<string | null>(null)
   const { headerTheme, setHeaderTheme } = useHeaderTheme()
-  const _pathname = usePathname()
+
+  const updateTheme = useEffectEvent(() => {
+    if (headerTheme && headerTheme !== theme) {
+      setTheme(headerTheme)
+    }
+  });
 
   useEffect(() => {
     setHeaderTheme(null)
-     
+
   }, [setHeaderTheme])
 
+
   useEffect(() => {
-    if (headerTheme && headerTheme !== theme) setTheme(headerTheme)
-     
-  }, [headerTheme, theme])
+    updateTheme();
+  }, [])
 
   return (
     <header className="sticky top-0 z-20" {...(theme ? { 'data-theme': theme } : {})}>
       <Container className="wg-grid-1 py-8">
         <Link href="/" className="col-span-4 flex justify-start">
           <div>
-          <Logo className="invert dark:invert-0" />
+            <Logo className="invert dark:invert-0" />
           </div>
         </Link>
         <div className="col-span-3"></div>
